7fcca2b04c40f9cd3d396a9184d6c405997c1f59
Outkarm <jkpordje@gmail.com>
2023-05-13 16:21:25 +0000

    update post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,11 @@
 class PostsController < ApplicationController
   def index
+    @users = User.find(params[:user_id])
     @posts = Post.all
   end
 
   def show
+    @users = User.find(params[:user_id])
     @posts = Post.find(params[:id])
   end
 end
